517ec43763beb73d61221387533a75ba9e9c76ef
mjfre <>
2019-05-25 13:17:32 -0700

    Added L4M2 Solution

--- Level4-Module2/src/StringMethods/StringMethods.java
+++ Level4-Module2/src/StringMethods/StringMethods.java
@@ -32,68 +32,118 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		return null;
+		return s1.length() >= s2.length() ? s1 : s2;
 	}
 
-	
-	// if String s contains the word "underscores", change all of the spaces to underscores
+	// if String s contains the word "underscores", change all of the spaces to
+	// underscores
 	public static String formatSpaces(String s) {
-		return null;
+		if (s.contains("underscores")) {
+			s = s.replace(' ', '_');
+		}
+		return s;
 	}
 
-	
-	// Return the name of the person whose LAST name would appear first if they were in alphabetical order
+	// Return the name of the person whose LAST name would appear first if they were
+	// in alphabetical order
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
-		return null;
+		s1 = s1.trim();
+		s2 = s2.trim();
+		s3 = s3.trim();
+
+		String s1LastName = s1.substring(s1.indexOf(" ") + 1);
+		String s2LastName = s2.substring(s2.indexOf(" ") + 1);
+		String s3LastName = s3.substring(s3.indexOf(" ") + 1);
+
+		if (s1LastName.compareTo(s2LastName) <= 0) {
+			if (s1LastName.compareTo(s3LastName) <= 0) {
+				return s1;
+			} else {
+				return s3;
+			}
+		} else {
+			if (s2LastName.compareTo(s3LastName) <= 0) {
+				return s2;
+			} else {
+				return s3;
+			}
+		}
 	}
-	
-	
+
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		return 0;
+		int total = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (Character.isDigit(s.charAt(i))) {
+				total += Character.getNumericValue(s.charAt(i));
+			}
+		}
+		return total;
 	}
-	
-	
+
 	// Return the number of times String substring appears in String s
 	public static int substringCount(String s, String substring) {
-		return 0;
+		int total = 0;
+		int startIndex = 0;
+		while (s.indexOf(substring, startIndex) >= 0) {
+			total++;
+			startIndex = s.indexOf(substring, startIndex) + 1;
+		}
+		return total;
 	}
 
 	// Call Utitilities.encrypt to encrypt String s
 	public static String encrypt(String s, char key) {
-		return null;
+		String output = Utilities.encrypt(s.getBytes(), (byte) key);
+		return output;
 	}
 
 	// Call Utilities.decrypt to decrypt the cyphertext
 	public static String decrypt(String s, char key) {
-		return null;
+		String output = Utilities.decrypt(s, (byte) key);
+		return output;
 	}
-
 
 	// Return the number of words in String s that end with String substring
 	// You can assume there are no punctuation marks between words
 	public static int wordsEndsWithSubstring(String s, String substring) {
-		return 0;
+		int count = 0;
+		String[] words = s.split(" ");
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].endsWith(substring)) {
+				count++;
+			}
+		}
+		return count;
 	}
-	
 
 	// Given String s, return the number of characters between the first occurrence
 	// of String substring and the final occurrence
 	// You can assume that substring will appear at least twice
 	public static int distance(String s, String substring) {
-		return 0;
+		return s.lastIndexOf(substring) - (s.indexOf(substring) + substring.length());
 	}
-
 
 	// Return true if String s is a palindrome
 	// palindromes are words or phrases are read the same forward as backward.
 	// HINT: ignore/remove all punctuation and spaces in the String
 	public static boolean palindrome(String s) {
+		String output = "";
+		for (int i = 0; i < s.length(); i++) {
+			if (Character.isLetter(s.charAt(i))) {
+				output += Character.toLowerCase(s.charAt(i));
+			}
+		}
+		for (int i = 0; i < output.length(); i++) {
+			if (output.charAt(i) != output.charAt(output.length() - 1 - i)) {
+				return false;
+			}
+		}
 		return true;
 	}
-	
+
 }
 
 class Utilities {
