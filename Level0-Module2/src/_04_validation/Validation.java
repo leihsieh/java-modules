6c742d48fa30fc7232d3a09378360e5d3d0acf14
OrigamiMM <ouacevedo@gmail.com>
2019-06-25 14:43:24 -0700

    Added Solution to L0M2_04

--- Level0-Module2/src/_04_validation/Validation.java
+++ Level0-Module2/src/_04_validation/Validation.java
@@ -11,13 +11,22 @@ public class Validation {
 	public static void main(String[] args) {
 		
 		Random randomMaker = new Random();
-		
-		int randomNumber = randomMaker.nextInt(5);
-		
-		System.out.println(randomNumber);
-
+		for(int i = 0; i < 10; i ++) {
+			int randomNumber = randomMaker.nextInt(5);
+			System.out.println(randomNumber);
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+   			 if(randomNumber == 0) {
+   				 JOptionPane.showMessageDialog(null, "You have a great smile!");
+   		 	}else if(randomNumber == 1) {
+   				 JOptionPane.showMessageDialog(null, "You are very cool!");
+   		 	}else if(randomNumber == 2) {
+   				 JOptionPane.showMessageDialog(null, "You are very smart!");
+   		 	}else if(randomNumber == 3) {
+   				 JOptionPane.showMessageDialog(null, "You are the best!");
+   		 	}else if(randomNumber == 4) {
+   				 JOptionPane.showMessageDialog(null, "You can do anything!");
+   		 	}
+   	 	}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
