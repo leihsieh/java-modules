6aafea53e4b5f1b3cc2e1f1e66954f1c3de2bcf8
OrigamiMM <ouacevedo@gmail.com>
2019-06-25 14:52:47 -0700

    Added Solution to L0M2_05

--- Level0-Module2/src/_05_magic_8_ball/Magic8Ball.java
+++ Level0-Module2/src/_05_magic_8_ball/Magic8Ball.java
@@ -8,28 +8,35 @@ import java.util.Random;
 
 public class Magic8Ball {
 
-	// 1. Make a main method that includes all the steps below….
-
-	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
-	// 3. Print out this variable
-
-	// 4. Get the user to enter a question for the 8 ball
-
-	// 5. If the random number is 0
-
-	// -- tell the user "Yes"
-
-	// 6. If the random number is 1
-
-	// -- tell the user "No"
-
-	// 7. If the random number is 2
-
-	// -- tell the user "Maybe you should ask Google?"
-
-	// 8. If the random number is 3
-
-	// -- write your own answer
-
+    // 1. Make a main method that includes all the steps belowâ€¦.
+    public static void main(String[] args) {
+	    
+   	 // 2. Make a variable that will hold a random number and put a random number
+   	 // into this variable using "new Random().nextInt(4)"
+   	 int randomNumber = new Random().nextInt(4);
+	    
+   	 // 3. Print out this variable
+   	 System.out.println(randomNumber);
+	    
+   	 // 4. Get the user to enter a question for the 8 ball
+   	 JOptionPane.showInputDialog(null, "What would you like to ask the 8 ball?");
+	    
+   	 // 5. If the random number is 0
+   	 // -- tell the user "Yes"
+   	 // 6. If the random number is 1
+   	 // -- tell the user "No"
+   	 // 7. If the random number is 2
+   	 // -- tell the user "Maybe you should ask Google?"
+   	 // 8. If the random number is 3
+   	 // -- write your own answer
+   	 if (randomNumber == 0) {
+   		 JOptionPane.showMessageDialog(null, "Yes");
+   	 } else if (randomNumber == 1) {
+   		 JOptionPane.showMessageDialog(null, "No");
+   	 } else if (randomNumber == 2) {
+   		 JOptionPane.showMessageDialog(null, "Maybe you should ask Google?");
+   	 } else if (randomNumber == 3) {
+   		 JOptionPane.showMessageDialog(null, "CUSTOM ANSWER HERE");
+   	 }
+    
 }
