a25f55026d8e21c392cd2411941538964a6e2a17
Ian Lee <ianbosslee@gmail.com>
2019-06-25 12:08:44 -0700

    Added Solution

--- Level0-Module0/src/_01_robot_square/RobotSquare.java
+++ Level0-Module0/src/_01_robot_square/RobotSquare.java
@@ -9,25 +9,25 @@ import org.jointheleague.graphical.robot.Robot;
 // This recipe draws a square using the Robot
 
 public class RobotSquare {
-    public static void main(String[] args) throws Exception {
- 
-    	// 1. Make a new Robot
+	public static void main(String[] args) throws Exception {
 
+		// 1. Make a new Robot
+		Robot r2d2 = new Robot();
 
-        // 3. Put the robot's pen down
+		// 3. Put the robot's pen down
+		r2d2.penDown();
 
+		// 6. Make the robot move as fast as possible
+		r2d2.setSpeed(100);
+		// 5. Do everything below here 4 times
 
-        // 6. Make the robot move as fast as possible
+		for (int i = 0; i < 4; i++) {
+			// 2. Move your robot 200 pixels
 
+			r2d2.move(200);
 
-        // 5. Do everything below here 4 times
-
-
-        //         2. Move your robot 200 pixels
-
-
-        //         4. Turn the robot 90 degrees to the right (90 degrees)
-
-
-    }
+			// 4. Turn the robot 90 degrees to the right (90 degrees)
+			r2d2.turn(90);
+		}
+	}
 }
