35d0505b0c269fb92e8abca343f8d9bf7cadc168
OrigamiMM <ouacevedo@gmail.com>
2019-07-01 19:33:12 -0700

    Added Solution to L0M2_08

--- Level0-Module2/src/_08_safe_cracker/SafeCracker.java
+++ Level0-Module2/src/_08_safe_cracker/SafeCracker.java
@@ -22,8 +22,9 @@ public class SafeCracker {
 		 * Your mission: use the tryCode method to crack the safe by trying all possible
 		 * combinations
 		 */
-		
-
+		for (int i = 9999000; i < 10000000; i++) {
+			tryCode(i);
+		}
 	}
 
 	static void tryCode(int guess) {
