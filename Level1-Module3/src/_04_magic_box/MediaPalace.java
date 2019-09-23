0e04e09682cd8fef4dfcdcb7441058031b566370
Daniel Commins <dencee@users.noreply.github.com>
2019-09-23 11:31:46 -0700

    Media Palace method clean up
    -Removed sounds methods that weren't working.
    +Added the mp3 player code from jukebox recipe to play mp3s.
    +Consolidated the loadImage methods into 1 method.

--- Level1-Module3/src/_04_magic_box/MediaPalace.java
+++ Level1-Module3/src/_04_magic_box/MediaPalace.java
@@ -1,109 +1,173 @@
 package _04_magic_box;
 /*
- *    Copyright (c) The League of Amazing Programmers 2013-2018
+ *    Copyright (c) The League of Amazing Programmers 2013-2019
  *    Level 1
  */
 
-
-
 import java.applet.AudioClip;
-import java.io.File;
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JApplet;
 import javax.swing.JLabel;
 
+import javazoom.jl.player.advanced.AdvancedPlayer;
+
 public class MediaPalace {
 
-	public JLabel loadImageFromTheInternet(String imageURL) throws MalformedURLException {
-		URL url = new URL(imageURL);
-		Icon icon = new ImageIcon(url);
-		return new JLabel(icon);
+    // This method loads an image onto a JLabel and checks for the image in the following order:
+    // 1) within the package, 2) on the hard drive, 3) on the Internet (URL).
+    // The input parameter is either a URL, location on the hard drive, or a location in the package
+    //   Example of a URL: https://www.oracle.com/a/ocom/img/hp11-intl-java-logo.jpg
+    //   Example of a location in the package: "hp11-intl-java-logo.jpg"
+    //   Example of a location in the hard drive: "/Users/leaguestudent/Desktop/hp11-intl-java-logo.jpg"
+	public JLabel loadImage(String imageLocationOrURL) {
+        URL url = null;
+        Icon icon = null;
+        JLabel labelWithImage = null;
+
+        // 1. Try to load image from within the project
+        url = getClass().getResource(imageLocationOrURL);
+        
+        if(url != null) {
+            icon = new ImageIcon(url);
+            labelWithImage = new JLabel(icon);
+        } else {
+            
+            // 2. Try and load image from the hard drive
+            icon = new ImageIcon(imageLocationOrURL);
+            
+            if(icon.getIconHeight() != -1 && icon.getIconWidth() != -1) {
+                labelWithImage = new JLabel(icon);
+            } else { 
+                
+                // 3. Try and load image from the Internet
+                try {
+                    url = new URL(imageLocationOrURL);
+                    icon = new ImageIcon(url);
+                    labelWithImage = new JLabel(icon);
+                } catch( MalformedURLException e ) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        return labelWithImage;
 	}
 
-	/*
-	 * To use this method, the image must be placed in your Eclipse project in the same package as this class.
-	 */
-	public JLabel loadImageFromWithinProject(String fileName) {
-		URL imageURL = getClass().getResource(fileName);
-		Icon icon = new ImageIcon(imageURL);
-		return new JLabel(icon);
+    void speak(String words) {
+        try {
+            Runtime.getRuntime().exec("say " + words).waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+	
+	// Use this method to play sound files with ".wav" at the end of the file name.
+	// Download .wav sound files at freesound.org
+	// freesound.org username: leagueofamazing password: code4life
+	public AudioClip playWavSoundFile(String fileName) {
+	    URL url = getClass().getResource(fileName);
+		AudioClip audio = JApplet.newAudioClip(url);
+		
+		audio.play();
+		
+		return audio;
+	}
+	
+	// Use this class to play sound files with a ".mp3" at the end of the file name.
+	public void playMp3SoundFile(String fileName, int durationSeconds) {
+	    Song song = new Song(fileName);
+	    song.setDuration( durationSeconds );
+	    song.play();
 	}
 
-	/*
-	 * To use this method, pass in the full path of the image.
-	 */
-	public JLabel loadImageFromHardDrive(String filePath) {
-		Icon icon = new ImageIcon(filePath);
-		return new JLabel(icon);
+	class Song {
+	    private int duration;
+	    private String songAddress;
+	    private AdvancedPlayer mp3Player;
+	    private InputStream songStream;
+
+	    /**
+	     * Songs can be constructed from files on your computer or Internet
+	     * addresses.
+	     * 
+	     * Examples: <code> 
+	     *      new Song("everywhere.mp3");     //from default package 
+	     *      new Song("/Users/joonspoon/music/Vampire Weekend - Modern Vampires of the City/03 Step.mp3"); 
+	     *      new Song("http://freedownloads.last.fm/download/569264057/Get%2BGot.mp3"); 
+	     * </code>
+	     */
+	    public Song(String songAddress) {
+	        this.songAddress = songAddress;
+	    }
+
+	    public void play() {
+	        loadFile();
+	        if (songStream != null) {
+	            loadPlayer();
+	            startSong();
+	        } else
+	            System.err.println("Unable to load file: " + songAddress);
+	    }
+
+	    public void setDuration(int seconds) {
+	        this.duration = seconds * 100;
+	    }
+
+	    public void stop() {
+	        if (mp3Player != null)
+	            mp3Player.close();
+	    }
+
+	    private void startSong() {
+	        Thread t = new Thread() {
+	            public void run() {
+	                try {
+	                    if (duration > 0)
+	                        mp3Player.play(duration);
+	                    else
+	                        mp3Player.play();
+	                } catch (Exception e) {
+	                }
+	            }
+	        };
+	        t.start();
+	    }
+
+	    private void loadPlayer() {
+	        try {
+	            this.mp3Player = new AdvancedPlayer(songStream);
+	        } catch (Exception e) {
+	        }
+	    }
+
+	    private void loadFile() {
+	        if (songAddress.contains("http"))
+	            this.songStream = loadStreamFromInternet();
+	        else
+	            this.songStream = loadStreamFromComputer();
+	    }
+
+	    private InputStream loadStreamFromInternet() {
+	        try {
+	            return new URL(songAddress).openStream();
+	        } catch (Exception e) {
+	            return null;
+	        }
+	    }
+
+	    private InputStream loadStreamFromComputer() {
+	        try {
+	            return new FileInputStream(songAddress);
+	        } catch (FileNotFoundException e) {
+	            return this.getClass().getResourceAsStream(songAddress);
+	        }
+	    }
 	}
-
-	/*
-	 * To use this method, you must first have the javazoom jar inthe project.
-	 * If this jar has not been added, you can download from: http://www.javazoom.net/javalayer/javalayer.html
-	 * Uncomment this method.
-	 */
-	// private void playMp3FromComputer(String fileName) throws JavaLayerException {
-	// FileInputStream songStream = new FileInputStream(fileName);
-	//
-	// final Player playMp3 = new Player(songStream);
-	//
-	// Thread t = new Thread() {
-	// public void run() {
-	// try {
-	// playMp3.play();
-	// } catch (JavaLayerException e) {
-	// // TODO Auto-generated catch block
-	// e.printStackTrace();
-	// }
-	// }
-	// };
-	// t.start();
-	// }
-
-	/* This method will use your default mp3 player to play the song */
-	public void playMusicOnComputer(String fileName) {
-		File fileToPlay = new File(fileName);
-		try {
-			java.awt.Desktop.getDesktop().open(fileToPlay);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-	}
-
-	/* If you want to use an mp3, you must first convert it to a .wav file on http://media.io */
-	public AudioClip loadSound(String fileName) {
-		return JApplet.newAudioClip(getClass().getResource(fileName));
-	}
-
-	public void playSoundFromInternet(String soundURL) {
-		try {
-			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new URL(soundURL));
-			Clip clip = AudioSystem.getClip();
-			clip.open(audioInputStream);
-			clip.start();
-			Thread.sleep(500);
-		} catch (Exception ex) {
-			System.out.println("Problem playing sound: " + soundURL);
-			ex.printStackTrace();
-		}
-	}
-
-	void speak(String words) {
-		try {
-			Runtime.getRuntime().exec("say " + words).waitFor();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 }
-
-
