72ea22319b75e8baddca8c50900d0d7680cbf834
jaLeague <jackie.arseneau@jointheleague.org>
2019-10-22 13:29:44 -0700

    MVC

--- Level1-Module0/src/_09_sound_effects_machine/SoundEffectsModel.java
+++ Level1-Module0/src/_09_sound_effects_machine/SoundEffectsModel.java
@@ -5,14 +5,16 @@ import java.applet.AudioClip;
 import javax.swing.JApplet;
 
 public class SoundEffectsModel {
+	
+	// 1. Add a variable for each of the sounds in your sound effects machine
 	private AudioClip drumSound;
 	
-	
-	
 	SoundEffectsModel() {
+		// 3. Add code here to initialize all the variables above with AudioClips
 		setDrumSound("drum.wav");
 	}
 	
+	// 2. Make copies of the following 2 methods to support each of your sound effects
 	public void setDrumSound(String drumSoundFile) {
 		drumSound = JApplet.newAudioClip(getClass().getResource(drumSoundFile));
 	}
