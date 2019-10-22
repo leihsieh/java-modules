c69d6bb9527edb4ad495d104efc50849c7544a03
jaLeague <jackie.arseneau@jointheleague.org>
2019-10-22 13:27:46 -0700

    MVC

--- Level1-Module0/src/_09_sound_effects_machine/SoundEffectsController.java
+++ Level1-Module0/src/_09_sound_effects_machine/SoundEffectsController.java
@@ -5,50 +5,40 @@ import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 
-public class SoundEffectsController implements MouseListener {
+public class SoundEffectsController  {
+	
+	// 1.  Make this class a MouseListener (Hint: Use implements MouseListener)
 
-	SoundEffectsModel model;
-	SoundEffectsView view;
+	// 2.  Let Eclipse assist you in adding the unimplemented methods that a MouseListener needs to have.
+	//     When you have done this, you should see 5 new methods below. Find the one called mouseClicked.
+	
+	private SoundEffectsModel model;
+	private SoundEffectsView view;
 	
 	SoundEffectsController(SoundEffectsView view) {	
-	/* The controller acts as the go-between so needs to access the view and model */	
+		
+		/* The controller acts as the go-between so needs to access the view and model */	
 	
-	this.view = view;
-	model =  new SoundEffectsModel();
+		this.view = view;
+		model =  new SoundEffectsModel();
 	}
+
 	
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		JButton labelClicked = (JButton) e.getSource(); 
-		System.out.println(labelClicked.getText());
-		if (labelClicked.getText().equals("Drum")) {
+	// 3. In the mouseClicked method, add code to get the button that the user clicked.
+	//    Hint: you will need to find the source of the MouseEvent
+	
+	// 4. Use the text of the button to decide which sound to play (use the playSoundForButton method below)
+
+	
+	public void playSoundForLabel (JButton button) {
+
+		// 5. Complete this method for all the sounds you have in your SoundEffectsModel
+		
+		
+		if (button.getText().equals("Drum")) {
+			
 			model.getDrumSound().play();
 		}
-	}
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
