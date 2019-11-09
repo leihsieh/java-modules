b0a690fb7a6a516f27adc68ba9d82a4e9e643aae
jaLeague <jackie.arseneau@jointheleague.org>
2019-11-08 16:04:39 -0800

    MVC

--- Level1-Module0/src/_09_sound_effects_machine/SoundEffectsController.java
+++ Level1-Module0/src/_09_sound_effects_machine/SoundEffectsController.java
@@ -7,19 +7,31 @@ import javax.swing.JButton;
 
 public class SoundEffectsController  {
 	
-	/** This class has been created but there is a lot of code missing. Check the recipe for instructions **/
+	/** The controller will play sounds when the buttons are pressed **/
 	
 	
-	/* declare view and model variables here */
+	/* 1. declare view and model variables here */
 
 	
 	SoundEffectsController(SoundEffects view) {	
 		
-		/* initialize the view and model variables here */
+		/* 2. initialize the view and model variables here */
 
 	}
 
+    // 3.  Make this class a MouseListener so it can process mouse clicks.
 	
+	// 4.  If you successfully made this a MouseListener, there should now be a mouseClicked method. 
+	//     Add code to this method for each of the buttons / sounds in the Sound Effects Machine.
+	//     Hint: use the button's text to figure out which one was clicked
+	
+	
+	
+	// 5.  Call a method on the Model to get the sound (AudioClip) that goes with the button, 
+	//     then play it.
+	
+	
+ 	// 6.  Go back to the View (SoundEffects class) and add this MouseListener to all of the buttons.
 
 
 }
