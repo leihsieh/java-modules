01c792a74e04fa131bb23a9482fcc0fb410d2e47
sebastiantroncoso <sctroncoso10@gmail.com>
2020-02-26 15:04:00 -0800

    Working on parseInt feature
    Creating two parts for this new parseInt introduction that includes Strings -> int and > , < , and +

---
+++ Level0-Module2/src/_14_intro_to_parseint/IntroToParseIntPartB.java
@@ -0,0 +1,24 @@
+package _14_intro_to_parseint;
+
+import javax.swing.JOptionPane;
+
+public class IntroToParseIntPartB {
+
+	public static void main(String[] args) {
+		
+		//Ask the user to enter their height in inches and set it to the height variable
+		String height = JOptionPane.showInputDialog("Enter height in inches");
+		
+		// uncomment the line below
+		//if( height > 60 ) //Do you see the red line?
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+} 
---
+++ Level0-Module2/src/_17_Movies/MovieWithFriends.java
@@ -0,0 +1,14 @@
+package _17_Movies;
+
+public class MovieWithFriends {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// Enter the name of
+		
+
+	}
+
+}
