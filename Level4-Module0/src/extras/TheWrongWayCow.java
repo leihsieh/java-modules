cb97d927fa24b11183721daca81f054c2d6a6720
Daniel Commins <dencee@users.noreply.github.com>
2020-03-03 12:25:54 -0800

    Added another solution to Wrong Way Cow

--- Level4-Module0/src/extras/TheWrongWayCow.java
+++ Level4-Module0/src/extras/TheWrongWayCow.java
@@ -1,5 +1,85 @@
 
 public class TheWrongWayCow {
+    
+    public static int[] findWrongWayCow( final char[][] field ) {
+        int[] rowCol = null;
+        int northCnt = 0;
+        int southCnt = 0;
+        int westCnt = 0;
+        int eastCnt = 0;
+        
+        HashMap<String, int[]> hm = new HashMap<String, int[]>(4);
+        
+        for( int i = 0; i < field.length; i++ ) {
+            for( int j = 0; j < field[i].length; j++ ) {
+                
+                if( field[i][j] == 'c' ) {
+                    int numRows = field.length;
+                    int numCols = field[i].length;
+                    
+                    // North
+                    if( northCnt < 2 ) {
+                        if( i + 2 < numRows && field[i+1][j] == 'o' && field[i+2][j] == 'w' ) {
+                            hm.put( "north", new int[] { j, i } );
+                            northCnt++;
+                            continue;
+                        }
+                    }
+                    
+                    // South
+                    if( southCnt < 2 ) {
+                        if( i - 2 >= 0 && field[i-1][j] == 'o' && field[i-2][j] == 'w' ) {
+                            hm.put( "south", new int[] { j, i } );
+                            southCnt++;
+                            continue;
+                        }
+                    }
+                    
+                    // West
+                    if( westCnt < 2 ) {
+                        if( j + 2 < numCols && field[i][j+1] == 'o' && field[i][j+2] == 'w' ) {
+                            hm.put( "west", new int[] { j, i } );
+                            westCnt++;
+                            continue;
+                        }
+                    }
+                    
+                    // East
+                    if( eastCnt < 2 ) {
+                        if( j - 2 >= 0 && field[i][j-1] == 'o' && field[i][j-2] == 'w' ) {
+                            hm.put( "east", new int[] { j, i } );
+                            eastCnt++;
+                            continue;
+                        }
+                    }
+                    
+                    // If there was a 'c' it must have matched one of the directions,
+                    // so check if the search should continue
+                    if( hm.size() > 1 && ( northCnt + southCnt + eastCnt + westCnt ) > 2 ) {
+                        
+                        // Set i to break out of outer loop
+                        i = field.length;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        // Which ever direction has only 1 cow, that's the WWC!
+        if( northCnt == 1 ) {
+            rowCol = hm.get( "north" );
+        } else if( southCnt == 1 ) {
+            rowCol = hm.get( "south" );
+        } else if( eastCnt == 1 ) {
+            rowCol = hm.get( "east" );
+        } else if( westCnt == 1 ) {
+            rowCol = hm.get( "west" );
+        }
+        
+        return rowCol;
+    }
+    
+/* Solution #2
     public static int[] findWrongWayCow(final char[][] field) {
         int numCows = 0;
         int xCowCoord = -1;
@@ -95,5 +175,5 @@ public class TheWrongWayCow {
 
         return new int[] { yCowCoord, xCowCoord };
     }
-
+*/
 }
