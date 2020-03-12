6cc06e7ac373c0828cee97ec91953cb45b177af6
sebastiantroncoso <sctroncoso10@gmail.com>
2020-03-11 18:42:28 -0700

    Finishing touches on StringsCannotAdd and LegolandRollerCoaster

---
+++ Level0-Module2/src/_14_intro_to_parseint/LegolandRollerCoaster.java
@@ -0,0 +1,24 @@
+package _14_intro_to_parseint;
+
+import javax.swing.JOptionPane;
+
+public class LegolandRollerCoaster {
+
+	/* 1) Make a main method that includes all the steps below…. */
+		
+		/* 2) Ask the user to enter their height in inches using JOptionPane and set it to the variable heightToRide */
+		
+		// Uncomment the line below...
+			//if( heightToRide > 42 ) { }
+		
+		//Do you see the heightToRide > 42 underlined red? That is because you are comparing a STRING to an INTEGER.
+		
+		/* 3) Convert heightToRide to heightAsInt using Integer.parseInt();  Hint: int heightAsInt = Integer.parse... */
+		
+		
+		/* 4) if heightAsInt is greater than 42 inches, print "You can ride the LEGO TECHNIC coaster!" using JOptionPane */
+		
+		
+		/* 5) if the heightAsInt is greater than 48 inches, print "Hurray! You are tall enough to ride the LEGO coaster alone!" */
+		
+} 
