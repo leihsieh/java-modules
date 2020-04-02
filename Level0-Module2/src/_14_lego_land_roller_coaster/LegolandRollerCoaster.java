711c19da5a4b44bfae592bba6c52263d3271e99f
sebastiantroncoso <sctroncoso10@gmail.com>
2020-04-02 13:02:53 -0700

    Changing small details in parseInt

--- Level0-Module2/src/_14_lego_land_roller_coaster/LegolandRollerCoaster.java
+++ Level0-Module2/src/_14_lego_land_roller_coaster/LegolandRollerCoaster.java
@@ -4,21 +4,24 @@ import javax.swing.JOptionPane;
 
 public class LegolandRollerCoaster {
 
-	/* 1) Make a main method that includes all the steps below…. */
+	// 1) Make a main method that includes all the steps below…. 
 		
-		/* 2) Ask the user to enter their height in inches using JOptionPane and set it to the variable heightToRide */
+		// 2) Ask the user to enter their height in inches using JOptionPane and set it to the variable heightToRide
 		
 		// Uncomment the line below...
 			//if( heightToRide > 42 ) { }
 		
 		//Do you see the heightToRide > 42 underlined red? That is because you are comparing a STRING to an INTEGER.
 		
-		/* 3) Convert heightToRide to heightAsInt using Integer.parseInt();  Hint: int heightAsInt = Integer.parse... */
+		// 3) Convert heightToRide to heightAsInt using Integer.parseInt();  Hint: int heightAsInt = Integer.parse... //
 		
 		
-		/* 4) if heightAsInt is greater than 42 inches, print "You can ride the LEGO TECHNIC coaster!" using JOptionPane */
+		// 4) if heightAsInt is greater than or equal to 42 inches, print "You can ride the LEGO TECHNIC coaster with someone else!" 
+		// Hint: use JOptionPane 
+	
+		// 5) if heightAsInt is less than 42 inches, print "You must be at least 42 inches tall to ride the roller coaster pal!"
 		
 		
-		/* 5) if the heightAsInt is greater than 48 inches, print "Hurray! You are tall enough to ride the LEGO coaster alone!" */
+		// 6) if the heightAsInt is greater than 48 inches, print "Hurray! You are tall enough to ride the LEGO coaster alone!" 
 		
 } 
--- Level0-Module2/src/_15_string_cannot_add/StringsCannotAdd.java
+++ Level0-Module2/src/_15_string_cannot_add/StringsCannotAdd.java
@@ -4,34 +4,34 @@ import javax.swing.JOptionPane;
 
 public class StringsCannotAdd {
 
-		/* 1) Make a main method that includes all the steps below…. */
+		// 1) Make a main method that includes all the steps below…. 
 		
-			/* 2) Ask the user to enter number 4 and hold it in the variable fourAsString */
+			// 2) Ask the user to enter number 4 and hold it in the variable fourAsString 
 			
-			/* 3) Ask the user to enter number 6 and hold it in the variable sixAsString */
+			// 3) Ask the user to enter number 6 and hold it in the variable sixAsString 
 			
-			/* 4) Print the addition of fourAsString and sizAsString using JOptionPane */
+			// 4) Print the addition of fourAsString and sizAsString using JOptionPane 
 			
 			// Did you notice 46 instead of 4 + 6 = 10?
 			
-			/* 5) Ask the user to enter number 5 and hold it in the variable fiveAsString */
+			// 5) Ask the user to enter number 5 and hold it in the variable fiveAsString 
 			
-			/* 6) Print the addition of the variables fourAsString, sixAsString, and fiveAsString */
+			// 6) Print the addition of the variables fourAsString, sixAsString, and fiveAsString 
 			
 			//Did you notice 456 instead of 4 + 6 + 5 = 15?
 			
 			// Insert fourAsString inside the parenthesis of Integer.parseInt(*fourAsString*) and set the method equal to fourAsInt
 			// Hint: int fourAsInt = Integer.parseInt(fourAsString);
 			
-			/* 7) Do the same for sixAsString. 
+			// 7) Do the same for sixAsString. 
 			
-			/* 8) Print the addition of fourAsInt and sixAsInt using JOptionPane */
+			// 8) Print the addition of fourAsInt and sixAsInt using JOptionPane 
 			
 			// Did you notice the value 10? If so, hurray! You just changed the type of your variable from String to int.
 			
-			/* 9) Convert fiveAsString to fiveAsInt using Integer.parseInt();  */ 
+			// 9) Convert fiveAsString to fiveAsInt using Integer.parseInt();  
 			
-			/* 10) Print the addition of fourAsInt, sixAsInt, and fiveAsInt using JOptionPane */
+			// 10) Print the addition of fourAsInt, sixAsInt, and fiveAsInt using JOptionPane 
 			
-			// Did your value 15 instead of 465?
+			// Is your value now 15 instead of 465?
 }
