be6d2a42fd2b4a1ac17878d8ce8ca23c3d4dcf03
Andrew DeHuff <55668557+KamiFightingSpirit@users.noreply.github.com>
2020-05-21 14:55:35 -0700

    Update HighLowGame.java

--- Level0-Module3/src/_01_else_if/_3_high_low/HighLowGame.java
+++ Level0-Module3/src/_01_else_if/_3_high_low/HighLowGame.java
@@ -6,24 +6,24 @@ import java.util.Random;
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
+		// 1. Change this line to give you a random number between 1 - 100. 
 		int random = new Random().nextInt(5);
 		
 		// 2. Print out the random variable above
 		
-		// 11. Repeat steps 1 to 10 ten times
+		// 3. Have steps 4 through 12 repeat ten times
 		
-			// 1. Ask the user for a guess using a pop-up window, and save their response 
+			// 4. Ask the user for a guess using a pop-up window, and save their response 
 
-			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
+			// 5. Convert the users’ answer to an int (Integer.parseInt(string))
 			
-			// 5. if the guess is correct
-				// 6. Win
-				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
-			// 7. if the guess is high
-				// 8. Tell them it's too high
-			// 9. if the guess is low
-				// 10. Tell them it's too low
+			// 6. If the guess is correct:
+				// 7. Tell the user that they won
+				// 8. Use "System.exit(0);" to quit the game if the user guessed the right answer.
+			// 9. If the guess is high:
+				// 10. Tell the user it's too high
+			// 11. If the guess is low:
+				// 12. Tell the user it's too low
 
 		// 13. Tell them they lose
 		
