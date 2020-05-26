648e35dea16ccef33eb4b93dd4c82458a2d26a8f
Daniel Commins <dencee@users.noreply.github.com>
2020-05-26 15:45:43 -0700

    Add alternate solution for L5M0 Directory Iterator

--- Level5-Module0/src/_04_Directory_Iteration/CopyrightAdder.java
+++ Level5-Module0/src/_04_Directory_Iteration/CopyrightAdder.java
@@ -5,27 +5,62 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class CopyrightAdder {
-	public static void main(String[] args) {
-		File src = new File("src");
-		File[] files = src.listFiles();
-		if (files != null) {
-			for (File f : files) {
-				File[] subFile = f.listFiles();
-				if (subFile != null) {
-					for (File f2 : subFile) {
-						String fileName = f2.getAbsolutePath();
-						if (fileName.substring(fileName.length() - 5, fileName.length()).equals(".java")) {
-							try {
-								FileWriter fw = new FileWriter(fileName, true);
-								fw.write("\n\n//Copyright © 2019 FirstName LastName");
-								fw.close();
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+    public static void main(String[] args) {
+        File src = new File("src");
+        File[] files = src.listFiles();
+        if (files != null) {
+            for (File f : files) {
+                File[] subFile = f.listFiles();
+                if (subFile != null) {
+                    for (File f2 : subFile) {
+                        String fileName = f2.getAbsolutePath();
+                        if (fileName.substring(fileName.length() - 5, fileName.length()).equals(".java")) {
+                            try {
+                                FileWriter fw = new FileWriter(fileName, true);
+                                fw.write("\n\n//Copyright © 2019 FirstName LastName");
+                                fw.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        // Alternate solution using recursion
+        //new DirectoryIterator().addCopyright( "" );
+    }
+    
+    public void addCopyright( String path ) {
+
+        if( path.isEmpty() ) {
+            path = "src";
+        }
+        
+        File directory = new File( path );
+        File[] files = directory.listFiles();
+        
+        if( files != null ) {
+            for( File eachFile : files ) {
+                if( eachFile.isDirectory() ) {
+                    addCopyright( eachFile.getAbsolutePath() );
+                } else {
+                    if( eachFile.getName().endsWith( ".java" ) ) {
+                        writeCopyright( eachFile );
+                    }
+                }
+            }
+        }
+    }
+    
+    public void writeCopyright( File file ) {
+        try {
+            FileWriter fWriter = new FileWriter( file, true );
+            fWriter.write( "\n// Copyright © 2020 The League of Amazing Programmers" );
+            fWriter.close();
+        } catch( IOException e ) {
+            e.printStackTrace();
+        }
+    }
 }
