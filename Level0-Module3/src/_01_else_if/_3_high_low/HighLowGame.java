a45e9d7d8b84a4842fc50d08808bfa3eee39cdaa
Andrew DeHuff <55668557+KamiFightingSpirit@users.noreply.github.com>
2020-06-16 13:58:19 -0700

    Update HighLowGame.java (#158)
    * Update HighLowGame.java
    
    * Updated per my additional comments
    
    Changing the order to accomplish the twin goals of not having incorrect code run multiple times and provide clear on the instructions
    
    * Separate out declarations/instantiations
    
    1. Separated out the creation of the Random object and the declaration of the integer randomNum. 
    2. Tasked the student with calling the object to instantiate randomNum.

--- Level0-Module3/src/_01_else_if/_3_high_low/HighLowGame.java
+++ Level0-Module3/src/_01_else_if/_3_high_low/HighLowGame.java
@@ -6,24 +6,25 @@ import java.util.Random;
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		// 1. Using the Random object, ran, initialize the randomNumber variable to a number between 1 and 100.
+		Random ran = new Random();
+		int randomNumber;		
 		
 		// 2. Print out the random variable above
-		
-		// 11. Repeat steps 1 to 10 ten times
-		
-			// 1. Ask the user for a guess using a pop-up window, and save their response 
+
+			// 3. Ask the user for a guess using a pop-up window, and save their response 
 
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			
-			// 5. if the guess is correct
-				// 6. Win
-				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
-			// 7. if the guess is high
-				// 8. Tell them it's too high
-			// 9. if the guess is low
-				// 10. Tell them it's too low
+
+			// 5. If the guess is correct:
+				// 6. Tell the user that they won
+				// 7. Use "System.exit(0);" to quit the game if the user guessed the right answer.
+			// 8. If the guess is high:
+				// 9. Tell the user it's too high
+			// 10. If the guess is low:
+				// 11. Tell the user it's too low
+		
+		// 12. Have steps 3 through 11 repeat ten times
 
 		// 13. Tell them they lose
 		
